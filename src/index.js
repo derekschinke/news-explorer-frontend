314d3a7d186fdcd810cd873a4f3d34522ea54cf3
Derek Schinke <derekschinke@me.com>
2021-05-05 20:23:54 -0400

    fix: normalize.css order

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import '@csstools/normalize.css';
+import './index.css';
 import App from './components/App/App';
 import reportWebVitals from './reportWebVitals';
 
