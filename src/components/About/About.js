e02fe081975a2a763df6918a1ae1bdb31335f44c
Derek Schinke <derekschinke@me.com>
2021-05-11 23:00:54 -0400

    fix: About content

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -6,12 +6,7 @@ function About() {
     <section className="About">
       <div className="About__items">
         <img className="About__image" alt="" src={image}></img>{' '}
-        {/* <h2 className="About__header">About the author</h2> */}
-        {/* <p className="About__description">
-          This block describes the project author. Here you should indicate your
-          name, what you do, and which development technologies you know.
-        </p> */}
-        {/* <div className="About__text">
+        <div className="About__text">
           <h2 className="About__header">About the author</h2>
           <p className="About__description">
             This block describes the project author. Here you should indicate
@@ -21,7 +16,7 @@ function About() {
             You can also talk about your experience with Practicum, what you
             learned there, and how you can help potential customers.
           </p>
-        </div> */}
+        </div>
       </div>
     </section>
   );
