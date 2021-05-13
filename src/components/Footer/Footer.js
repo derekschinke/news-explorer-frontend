7010b378243b649f21aa1fb6dbe7b623ff10beba
Derek Schinke <derekschinke@me.com>
2021-05-12 20:55:58 -0400

    feature: Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,36 @@
 import './Footer.css';
 
+import gitHubLogo from '../../images/logos/github.svg';
+import linkedInLogo from '../../images/logos/linkedin.svg';
+
 function Footer() {
   return (
     <footer className="Footer">
-      <p className="Footer__copyright">© 2021 Supersite, Powered by News API</p>
+      <div className="Footer__block">
+        <p className="Footer__copyright">
+          © 2021 Derek Schinke, Powered by News API
+        </p>
+        <nav>
+          <ul className="Footer__list">
+            <li className="Footer__list-item">Home</li>
+            <li className="Footer__list-item">
+              <a className="Footer__link" href="https://practicum.yandex.com/">
+                Practicum by Yandex
+              </a>
+            </li>
+            <li className="Footer__list-item">
+              <a href="https://github.com/derekschinke">
+                <img className="Footer__icon" src={gitHubLogo} alt=""></img>
+              </a>
+            </li>
+            <li className="Footer__list-item">
+              <a href="https://www.linkedin.com/in/derekschinke">
+                <img className="Footer__icon" src={linkedInLogo} alt=""></img>
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </footer>
   );
 }
