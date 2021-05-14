8abaf9b179e06e5751c789dab870f9f2340c735c
Derek Schinke <derekschinke@me.com>
2021-05-13 23:15:09 -0400

    fix: About and Footer

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,19 +4,17 @@ import image from '../../images/jakayla-toney-LmF4eBYs08c-unsplash.webp';
 function About() {
   return (
     <section className="About">
-      <div className="About__items">
-        <img className="About__image" alt="" src={image}></img>{' '}
-        <div className="About__text">
-          <h2 className="About__header">About the author</h2>
-          <p className="About__description">
-            This block describes the project author. Here you should indicate
-            your name, what you do, and which development technologies you know.
-            <br />
-            <br />
-            You can also talk about your experience with Practicum, what you
-            learned there, and how you can help potential customers.
-          </p>
-        </div>
+      <img className="About__image" alt="" src={image}></img>{' '}
+      <div className="About__text">
+        <h2 className="About__header">About the author</h2>
+        <p className="About__description">
+          This block describes the project author. Here you should indicate your
+          name, what you do, and which development technologies you know.
+          <br />
+          <br />
+          You can also talk about your experience with Practicum, what you
+          learned there, and how you can help potential customers.
+        </p>
       </div>
     </section>
   );
