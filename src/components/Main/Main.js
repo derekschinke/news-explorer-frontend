7111f01bc4e2e0b87e71d9fcee46b17ab12412b9
Derek Schinke <derekschinke@me.com>
2021-05-15 02:00:27 -0400

    feature: SearchResults import in Main

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,9 +1,11 @@
 import About from '../About/About';
+import SearchResults from '../SearchResults/SearchResults';
 
 function Main() {
   return (
     <main>
       <About />
+      <SearchResults />
     </main>
   );
 }
