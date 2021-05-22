0f3ca85ed8a11242cf40632e55aeb1d575f49453
Derek Schinke <derekschinke@me.com>
2021-05-21 23:39:50 -0400

    feature: SavedNewsHeader

--- src/components/SavedNewsHeader/SavedNewsHeader.js
+++ src/components/SavedNewsHeader/SavedNewsHeader.js
@@ -1,9 +1,20 @@
+import styles from './SavedNewsHeader.module.css';
+
 import Navigation from '../Navigation/Navigation';
 
 function SavedNewsHeader() {
   return (
     <header>
       <Navigation />
+      <section className={styles.block}>
+        <div className={styles.items}>
+          <h2 className={styles.header}>Saved articles</h2>
+          <h3 className={styles.subheader}>Elise, you have 5 saved articles</h3>
+          <p className={styles.info}>
+            By keywords: <b>Nature, Yellowstone, and 2 other</b>
+          </p>
+        </div>
+      </section>
     </header>
   );
 }
