44e0ed9089a4afe9cdc2853cf8476c216f0e72ba
Derek Schinke <derekschinke@me.com>
2021-05-21 22:27:27 -0400

    feature: "Show more" button

--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -1,12 +1,17 @@
 import NewsCardList from '../NewsCardList/NewsCardList';
 import styles from './SearchResults.module.css';
 
+import classnames from 'classnames';
+
 function SearchResults() {
   return (
     <section className={styles.block}>
       <div className={styles.items}>
         <h2 className={styles.header}>Search results</h2>
         <NewsCardList />
+        <button className={classnames(styles.button, 'clickable')}>
+          Show more
+        </button>
       </div>
     </section>
   );
