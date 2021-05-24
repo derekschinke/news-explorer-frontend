3501b8851ff99c89a2903db6ca24470a0ce2b594
Derek Schinke <derekschinke@me.com>
2021-05-23 23:01:54 -0400

    feature: map cards

--- src/components/SavedNews/SavedNews.js
+++ src/components/SavedNews/SavedNews.js
@@ -2,11 +2,13 @@ import styles from './SavedNews.module.css';
 
 import NewsCardList from '../NewsCardList/NewsCardList';
 
+import { cards } from '../../utils/constants';
+
 function SavedNews() {
   return (
     <main className={styles.block}>
       <div className={styles.items}>
-        <NewsCardList />
+        <NewsCardList cards={cards} />
       </div>
     </main>
   );
--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -3,12 +3,14 @@ import styles from './SearchResults.module.css';
 
 import classnames from 'classnames';
 
+import { cards } from '../../utils/constants';
+
 function SearchResults() {
   return (
     <section className={styles.block}>
       <div className={styles.items}>
         <h2 className={styles.header}>Search results</h2>
-        <NewsCardList isMain={true} />
+        <NewsCardList isMain={true} cards={cards} />
         <button
           className={classnames(styles.button, 'clickable')}
           label="Show more"
