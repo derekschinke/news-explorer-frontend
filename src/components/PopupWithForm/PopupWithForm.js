fbf8f097fda2b16032341ccb337827f10f91cc7e
Derek Schinke <derekschinke@me.com>
2021-05-25 18:56:32 -0400

    fix: button enabling logic in CSS

--- src/components/PopupWithForm/PopupWithForm.js
+++ src/components/PopupWithForm/PopupWithForm.js
@@ -9,40 +9,38 @@ function PopupWithForm() {
         <div className={styles.content}>
           <h2 className={styles.header}>Sign in</h2>
           <form className={styles.form} method="POST">
-            <label className={styles.label}>
+            <label className={styles.label} htmlFor="email">
               Email
-              <br />
-              <input
-                className={styles.input}
-                type="email"
-                placeholder="Enter email"
-                required
-              />
-              <span className={styles.validation}>Invalid email address</span>
             </label>
+            <input
+              className={styles.input}
+              type="email"
+              placeholder="Enter email"
+              required
+              id="email"
+            />
+            <span className={styles.validation}>Invalid email address</span>
 
-            <label className={styles.label}>
+            <label className={styles.label} htmlFor="password">
               Password
-              <br />
-              <input
-                className={styles.input}
-                type="password"
-                placeholder="Enter password"
-                required
-                minLength="8"
-              />
-              <span className={styles.validation}>Invalid password</span>
             </label>
+            <input
+              className={styles.input}
+              type="password"
+              placeholder="Enter password"
+              required
+              minLength="8"
+              id="password"
+            />
+            <span className={styles.validation}>Invalid password</span>
 
-            <label>
-              <input
-                className={classnames(styles.submitButton)}
-                type="submit"
-                value="Sign in"
-                // disabled
-              />
-            </label>
+            <input
+              className={classnames(styles.submitButton)}
+              type="submit"
+              value="Sign in"
+            />
           </form>
+
           <p className={styles.redirect}>
             or{' '}
             <button className={classnames(styles.redirectButton, 'clickable')}>
