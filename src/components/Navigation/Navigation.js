f4e42b143719a80d067ba354a40a00db9d22a9b3
Derek Schinke <derekschinke@me.com>
2021-05-25 23:59:12 -0400

    fix: navigation button clickable

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -76,7 +76,7 @@ function Navigation(props) {
               </ul>
             </nav>
             <button
-              className={classnames(styles.button, '', {
+              className={classnames(styles.button, 'clickable', {
                 [styles.button_route_savedNews]: !props.isMain,
               })}
               label={props.isMain ? 'Sign in' : 'Sign out'}
