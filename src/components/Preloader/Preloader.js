d1b82c1ca1aa9c58a923006474662936c8e49fa9
Derek Schinke <derekschinke@me.com>
2021-05-28 13:58:15 -0400

    fix: Preloader className

--- src/components/Preloader/Preloader.js
+++ src/components/Preloader/Preloader.js
@@ -2,8 +2,8 @@ import styles from './Preloader.module.css';
 
 function Preloader() {
   return (
-    <section class={styles.block}>
-      <div class={styles.circle}></div>
+    <section className={styles.block}>
+      <div className={styles.circle}></div>
       <p className={styles.description}>Searching for news…</p>
     </section>
   );
