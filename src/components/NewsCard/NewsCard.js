f2988aa84da5a3f3638bb8a1a4d8fc13abafb2dc
Derek Schinke <derekschinke@me.com>
2021-05-29 00:24:09 -0400

    feature: bookmark icon toggle

--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -1,8 +1,20 @@
 import styles from './NewsCard.module.css';
 
 import classnames from 'classnames';
+import { useContext, useState } from 'react';
+import IsLoggedInContext from '../../contexts/IsLoggedInContext';
 
 function NewsCard(props) {
+  const isLoggedIn = useContext(IsLoggedInContext);
+
+  const [isBookmarked, setIsBookmarked] = useState(false);
+
+  function handleButtonClick() {
+    if (props.isMain) {
+      setIsBookmarked(!isBookmarked);
+    }
+  }
+
   return (
     <li className={styles.block}>
       <div
@@ -20,19 +32,25 @@ function NewsCard(props) {
             {props.card.keyword}
           </div>
           <button
-            className={classnames(styles.button, 'clickable', {
+            className={classnames(styles.button, {
+              clickable: !(props.isMain && !isLoggedIn),
               [styles.button_bookmark]: props.isMain,
+              [styles.button_bookmark_bookmarked]: props.isMain && isBookmarked,
               [styles.button_delete]: !props.isMain,
             })}
             label={props.isMain ? 'Bookmark' : 'Delete'}
+            disable={!(props.isMain && !isLoggedIn)}
+            onClick={handleButtonClick}
           ></button>
-          <div
-            className={classnames(styles.tooltip, {
-              [styles.tooltip_route_savedNews]: !props.isMain,
-            })}
-          >
-            {props.isMain ? 'Sign in to save articles' : 'Remove from saved'}
-          </div>
+          {!(props.isMain && isLoggedIn) && (
+            <div
+              className={classnames(styles.tooltip, {
+                [styles.tooltip_route_savedNews]: !props.isMain,
+              })}
+            >
+              {props.isMain ? 'Sign in to save articles' : 'Remove from saved'}
+            </div>
+          )}
         </div>
       </div>
       <div className={styles.info}>
