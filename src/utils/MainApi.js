caac38a8c3b0c94e43c962a391e9564d2ceadfb1
Derek Schinke <derekschinke@me.com>
2021-06-05 00:54:50 -0400

    feature: signIn function

--- src/utils/MainApi.js
+++ src/utils/MainApi.js
@@ -17,10 +17,38 @@ class MainApi {
       });
 
       if (!res.ok) {
-        throw new Error('Registration failed');
+        throw new Error('Sign up failed');
       }
 
       return res.json();
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  async signIn(email, password) {
+    try {
+      const res = await fetch(`${this.baseUrl}/signin`, {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email, password }),
+      });
+
+      if (!res.ok) {
+        throw new Error('Sign in failed');
+      }
+
+      const data = await res.json();
+
+      if (!data.token) {
+        throw new Error('No token received');
+      }
+
+      localStorage.setItem('token', data.token);
+      return data;
     } catch (err) {
       console.log(err);
     }
